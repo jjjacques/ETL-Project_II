9ea3c8ff0f643461b88f503244c8e048ad4e924a
JJJDataGuru <jjjacques@gmail.com>
2019-04-17 14:08:41 -0400

    Release Candidate

--- twiter_stock.sql
+++ twiter_stock.sql
@@ -2,35 +2,40 @@ CREATE DATABASE twitter_stocks_db;
 use twitter_stocks_db;
 
 CREATE TABLE summary (
-Ticker varchar(256),
-Close_Price double,
+Ticker varchar(256) PRIMARY KEY,
+Close_Price float,
 Volume float,
-Mkt_Cap INT,
-EPS INT,
-Earnings_Date INT
+Mkt_Cap float,
+EPS float,
+Earnings_Date float
 );
 
 CREATE TABLE profile (
-Ticker varchar(256),
+Ticker varchar(256) PRIMARY KEY,
 Company_Name varchar(256),
 Description blob,
-Telephone INT,
+Telephone float,
 Address varchar(256),
 Key_Execs_Name varchar(256),
 Key_Execs_Title varchar(256),
-Key_Execs_Salary INT,
-Mkt_Cap INT,
-EPS INT,
-Earnings_Date INT,
+Key_Execs_Salary float,
+Mkt_Cap float,
+EPS float,
+Earnings_Date float,
 Web_URL varchar(256)
 );
 
 CREATE TABLE sustainability (
-Ticker varchar(256),
-Total_ESG_Score INT,
-AVG_Perf_Env INT,
-Social_Score INT,
-Governance_Score INT
+Ticker varchar(256) PRIMARY KEY,
+Total_ESG_Score float,
+AVG_Perf_Env float,
+Social_Score float,
+Governance_Score float
 );
+
+
+-- Testing
+-- ALTER TABLE summary
+-- MODIFY COLUMN Earnings_Date date;
 
 SELECT * FROM sustainability
